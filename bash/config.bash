c2f15ef90134b6fe3a44f2d43d02861d6afc2a7f
Jason Garber <jason@sixtwothree.org>
2011-05-10 10:42:27 -0400

    Added history file controls to bash settings.

--- bash/config.bash
+++ bash/config.bash
@@ -1,6 +1,9 @@
 export EDITOR="mate -w"
-export HISTCONTROL=erasedups
 export PATH="/usr/local/bin:$PATH"
+
+HISTCONTROL=erasedups
+HISTFILE=~/.bash_history
+HISTSIZE=100
 
 PROMPT_COMMAND='echo -ne "\033]0;${PWD/$HOME/~}\007"'
 PS1='\[\033[1;36m\]\h: \[\033[00m\]\w\[\033[0;33m\]$(__git_ps1)\[\033[00m\] \u \$ '
