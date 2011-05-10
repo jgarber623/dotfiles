f23b95266b4912d270473ad25cefafae62b59e07
Jason Garber <jason@sixtwothree.org>
2011-05-10 09:04:35 -0400

    Added remainder of gitconfig to file.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,1 +1,12 @@
 export EDITOR="mate -w"
+export PATH="/usr/local/bin:$PATH"
+
+fpath=($ZSH/zsh/functions $fpath)
+
+autoload -U $ZSH/zsh/functions/*(:t)
+
+HISTFILE=~/.zsh_history
+HISTSIZE=100
+SAVEHIST=100
+
+setopt SHARE_HISTORY
