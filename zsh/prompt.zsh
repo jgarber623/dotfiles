5fbcb5085ac74efabf7bc23f84cdb9231b91bde7
Jason Garber <jason@sixtwothree.org>
2011-05-11 17:20:01 -0400

    Changed prefix and suffix characters.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -11,7 +11,7 @@ function prompt_char() {
 
 export PROMPT='%{$fg_bold[white]%}%n%{$reset_color%} at %{$fg_bold[cyan]%}%m%{$reset_color%}: $(collapse_pwd) $(git_prompt_info) $(prompt_char) '
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_no_bold[yellow]%}("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_no_bold[yellow]%}‹"
+ZSH_THEME_GIT_PROMPT_SUFFIX="›%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=" ✓"
 ZSH_THEME_GIT_PROMPT_DIRTY=" ✗"
