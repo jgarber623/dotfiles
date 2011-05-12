8d30cbd61d21d134a0c30a940eadf1afb90c6669
Jason Garber <jason@sixtwothree.org>
2011-05-11 21:12:43 -0400

    Added Apache aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,8 @@
+# Shortcuts to project files
 alias dotmate="mate $ZSH"
 alias dotcd="cd $ZSH"
+
+# Apache
+alias webstart="sudo apachectl start"
+alias webstop="sudo apachectl stop"
+alias webrestart="sudo apachectl restart"
