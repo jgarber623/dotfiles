c9196992125447d201ae8eab06791b11ed965fa2
Jason Garber <jason@sixtwothree.org>
2011-05-17 09:42:09 -0400

    Added function to set terminal window title.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -26,3 +26,10 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+
+# Set terminal window title to display current path
+case $TERM in
+	xterm*)
+		precmd () {print -Pn "\e]0;%~\a"}
+	;;
+esac
