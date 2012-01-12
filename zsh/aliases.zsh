b8e51c2a936f19cd3fc3e9a62e13ea7aaafd9057
Jason Garber <jason@sixtwothree.org>
2012-01-12 14:43:03 -0500

    Added an alias to copy an IP address to the clipboard.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,3 +16,6 @@ alias mystop="mysql.server stop"
 # PostgreSQL
 alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
+
+# Miscellaneous
+alias ipcopy="ipconfig getifaddr en1 | pbcopy"
