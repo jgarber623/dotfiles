a979522dc60c3e89c234662fa39ebb40e201bacb
Jason Garber <jason@sixtwothree.org>
2012-12-23 18:15:40 -0500

    More notes about things that didn't work.

--- osx/config.sh
+++ osx/config.sh
@@ -63,10 +63,10 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
 
 # Automatically illuminate built-in MacBook keyboard in low light
-defaults write com.apple.BezelServices kDim -bool true
+# FIX defaults write com.apple.BezelServices kDim -bool true
 
 # Turn off keyboard illumination when computer is not used for 5 minutes
-defaults write com.apple.BezelServices kDimTime -int 300
+# FIX defaults write com.apple.BezelServices kDimTime -int 300
 
 ###############################################################################
 # Screen                                                                      #
