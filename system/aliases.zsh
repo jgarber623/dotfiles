cdf024ff02c07872ecc13477f85aba7437b1c033
Jason Garber <jason@sixtwothree.org>
2014-04-21 09:33:07 -0400

    Change PostgreSQL alias.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -15,7 +15,7 @@ alias amgo='webgo && mygo'
 alias amstop='webstop && mystop'
 
 # PostgreSQL
-alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias pggo='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
 # Miscellaneous aliases
