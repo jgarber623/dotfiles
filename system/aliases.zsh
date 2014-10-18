f1e88635ee54a4a8fb67a5ce34079f0a93c74ea0
Jason Garber <jason@sixtwothree.org>
2014-10-18 14:18:18 -0400

    Comment out most aliases for now.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,24 +3,24 @@ alias dotcd='cd $ZSH'
 alias dotmate='subl $ZSH'
 
 # Apache and MySQL-related aliases
-alias ago='sudo apachectl start'
-alias astop='sudo apachectl stop'
-alias arestart='sudo apachectl restart'
+# alias ago='sudo apachectl start'
+# alias astop='sudo apachectl stop'
+# alias arestart='sudo apachectl restart'
 
-alias mygo='mysql.server start'
-alias mystop='mysql.server stop'
+# alias mygo='mysql.server start'
+# alias mystop='mysql.server stop'
 
-alias amgo='ago && mygo'
-alias amstop='astop && mystop'
+# alias amgo='ago && mygo'
+# alias amstop='astop && mystop'
 
 # PostgreSQL
-alias pggo='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+# alias pggo='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+# alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
 # Miscellaneous aliases
-alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
-alias ipcp='ipconfig getifaddr en0 | tr -d "\n" | pbcopy'
-alias ipshow='ipconfig getifaddr en0'
+# alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+# alias ipcp='ipconfig getifaddr en0 | tr -d "\n" | pbcopy'
+# alias ipshow='ipconfig getifaddr en0'
 alias ls='ls -flaG'
-alias mate='subl' #lolforever
-alias pubkey='more ~/.ssh/id_rsa.pub | pbcopy | echo "=> Public key copied to pasteboard."'
+# alias mate='subl' #lolforever
+# alias pubkey='more ~/.ssh/id_rsa.pub | pbcopy | echo "=> Public key copied to pasteboard."'
