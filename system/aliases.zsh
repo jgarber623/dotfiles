aca9e7968fde867e4ebc27811ddd6ca94709f36e
Jason Garber <jason@sixtwothree.org>
2014-10-19 01:37:38 -0400

    Re-enable some settings and aliases.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,11 +1,9 @@
 # dotfiles project aliases
 alias dotcd='cd $ZSH'
-alias dotmate='subl $ZSH'
+alias dotmate='atom $ZSH'
 
 # Apache and MySQL-related aliases
-# alias ago='sudo apachectl start'
-# alias astop='sudo apachectl stop'
-# alias arestart='sudo apachectl restart'
+alias apache='sudo apachectl'
 
 # alias mygo='mysql.server start'
 # alias mystop='mysql.server stop'
@@ -22,5 +20,5 @@ alias dotmate='subl $ZSH'
 # alias ipcp='ipconfig getifaddr en0 | tr -d "\n" | pbcopy'
 # alias ipshow='ipconfig getifaddr en0'
 alias ls='ls -flaG'
-# alias mate='subl' #lolforever
-# alias pubkey='more ~/.ssh/id_rsa.pub | pbcopy | echo "=> Public key copied to pasteboard."'
+alias mate='atom' #lolforever
+alias pubkey='more ~/.ssh/id_rsa.pub | pbcopy | echo "=> Public key copied to pasteboard."'
--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,2 @@
 # export CC="/usr/local/bin/gcc-4.2"
-# export EDITOR="subl -w"
+export EDITOR="atom -w"
