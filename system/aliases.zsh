d1e2da540dff1207361af1fa8a1b3883ccb8ebb2
Jason Garber <jason@sixtwothree.org>
2014-10-20 11:19:37 -0400

    Move back to Sublime Text. Add vhosts alias.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,9 +1,10 @@
 # dotfiles project aliases
 alias dotcd='cd $ZSH'
-alias dotmate='atom $ZSH'
+alias dotmate='subl $ZSH'
 
 # Apache and MySQL-related aliases
 alias apache='sudo apachectl'
+alias vhosts='subl /etc/apache2/extra/httpd-vhosts.conf'
 
 # alias mygo='mysql.server start'
 # alias mystop='mysql.server stop'
@@ -20,5 +21,5 @@ alias apache='sudo apachectl'
 # alias ipcp='ipconfig getifaddr en0 | tr -d "\n" | pbcopy'
 # alias ipshow='ipconfig getifaddr en0'
 alias ls='ls -flaG'
-alias mate='atom' #lolforever
+alias mate='subl' #lolforever
 alias pubkey='more ~/.ssh/id_rsa.pub | pbcopy | echo "=> Public key copied to pasteboard."'
--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,2 @@
 # export CC="/usr/local/bin/gcc-4.2"
-export EDITOR="atom -w"
+export EDITOR="subl -w"
