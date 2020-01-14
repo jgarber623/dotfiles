34580eea10d239ba122b7f648caf2d921056f1ee
Jason Garber <jason@sixtwothree.org>
2020-01-13 20:01:07 -0500

    Add NPM_CONFIG_INIT_* env vars

--- roles/zsh/files/.oh-my-zsh/custom/npm.zsh
+++ roles/zsh/files/.oh-my-zsh/custom/npm.zsh
@@ -1,2 +1,8 @@
 export NPM_CONFIG_DEPTH="0"
 export NPM_CONFIG_PROGRESS="false"
+
+export NPM_CONFIG_INIT_AUTHOR_NAME="Jason Garber"
+export NPM_CONFIG_INIT_AUTHOR_EMAIL="jason@sixtwothree.org"
+export NPM_CONFIG_INIT_AUTHOR_URL="https://sixtwothree.org"
+export NPM_CONFIG_INIT_LICENSE="MIT"
+export NPM_CONFIG_INIT_VERSION="0.1.0"
