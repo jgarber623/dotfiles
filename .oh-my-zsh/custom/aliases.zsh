630a4a5eecbd27cd745b672e9b022ed5774e0035
Jason Garber <jason@sixtwothree.org>
2022-05-27 16:27:06 -0400

    Use --location global flag in npm invocation

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,3 @@
 alias dotcd="cd ~/.dotfiles"
-alias outdated="brew outdated && nvm exec system npm outdated -g"
+alias outdated="brew outdated && nvm exec system npm outdated --location global"
 alias reload!="rehash"
