675e67ed3683fad9d45f33f4993c627f3ad8934d
Jason Garber <jason@sixtwothree.org>
2022-11-27 10:53:26 -0500

    Add Git production branch deploy function

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -1,3 +1,10 @@
+function git-production-branch-deploy() {
+  git checkout production
+  git merge main
+  git push
+  git checkout main
+}
+
 function touch() {
   mkdir -p "$(dirname "$1")"
   command touch "$1"
