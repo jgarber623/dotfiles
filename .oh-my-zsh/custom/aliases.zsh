6494e6e4a39948952681c25e8fc34ff71f78f15b
Jason Garber <jason@sixtwothree.org>
2023-09-05 12:45:09 -0400

    Add docker-clobber alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
+alias docker-clobber="docker system prune -f && docker volume prune -f && docker system prune -f"
 alias dotcd="cd ~/.dotfiles"
 alias outdated="brew outdated && nvm exec system npm outdated --location global"
 alias reload!="rehash"
