19100f321b4d161f35b4f9490a7bca81256347c9
Jason Garber <jason@sixtwothree.org>
2023-11-12 23:51:08 -0500

    Ditch superfluous comments

--- zsh/.config/zsh/plugins/rbenv/rbenv.plugin.zsh
+++ zsh/.config/zsh/plugins/rbenv/rbenv.plugin.zsh
@@ -1,6 +1,3 @@
 #!/usr/bin/env zsh
-#
-# rbenv.plugin.zsh - https://github.com/rbenv/rbenv
-#
 
 (( $+commands[rbenv] )) && eval "$(rbenv init - zsh)"
--- zsh/.config/zsh/plugins/starship/starship.plugin.zsh
+++ zsh/.config/zsh/plugins/starship/starship.plugin.zsh
@@ -1,6 +1,3 @@
 #!/usr/bin/env zsh
-#
-# starship.plugin.zsh - https://starship.rs
-#
 
 (( $+commands[starship] )) && eval "$(starship init zsh)"
