8aa6bb13c07970e95d9fcc61cea8fa836fa68e74
Jason Garber <jason@sixtwothree.org>
2023-11-22 22:14:08 -0500

    Configure ruby/debug history file

--- zsh/.config/zsh/configs/ruby.zsh
+++ zsh/.config/zsh/configs/ruby.zsh
@@ -5,3 +5,5 @@ export BUNDLE_USER_CACHE="${XDG_CACHE_HOME}/bundle"
 export BUNDLE_USER_CONFIG="${XDG_CONFIG_HOME}/bundle/config"
 
 export RBENV_ROOT="${XDG_DATA_HOME}/rbenv"
+
+export RUBY_DEBUG_HISTORY_FILE="${XDG_DATA_HOME}/rdbg/rdbg_history"
