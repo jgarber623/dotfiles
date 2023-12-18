d8915bd510270ed0b7a186fbd6a0b5b05c0b0ef6
Jason Garber <jason@sixtwothree.org>
2023-12-17 22:16:09 -0500

    Add xdg-utils note

--- zsh/.config/zsh/configs/browser.zsh
+++ zsh/.config/zsh/configs/browser.zsh
@@ -1,3 +1,8 @@
 #!/usr/bin/env zsh
 
-[[ "${OSTYPE}" == "darwin"* ]] && export BROWSER="${BROWSER:-open}"
+# On Debian/Ubuntu systems, the `open` command is part of the xdg-utils pakcage
+# and can be installed by running:
+#
+#   apt update && apt install --yes xdg-utils
+#
+export BROWSER="${BROWSER:-open}"
